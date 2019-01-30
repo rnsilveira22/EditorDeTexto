041702cfcc600372d35121d229801695556c29a8
rodrigo <rodrigo@rodrigo-HP-14-Notebook-PC>
2019-01-30 21:22:59 -0200

    terceiro comit

--- src/br/edu/editor/service/DocumentoService.java
+++ src/br/edu/editor/service/DocumentoService.java
@@ -1,31 +1,42 @@
 package br.edu.editor.service;
 
+import java.util.ArrayList;
+
 import br.edu.editor.model.Documento;
 
 public class DocumentoService {
-	
-	private boolean validaTitulo (String titulo){
-		if (titulo == null ||titulo==""||titulo.length()>50) {
+
+	private boolean validaTitulo(String titulo) {
+		if (titulo == null || titulo == "" || titulo.length() > 50) {
 			System.out.println("Titulo Invalido");
 			return false;
 		}
 		return true;
 	}
-	private boolean validaCorpo (String corpo){
-		if (corpo == null ||corpo==""||corpo.length()>500) {
+
+	private boolean validaCorpo(String corpo) {
+		if (corpo == null || corpo == "" || corpo.length() > 500) {
 			System.out.println("Corpo Invalido");
 			return false;
 		}
 
 		return true;
 	}
-	
+
 	public Documento salvar(Documento doc) {
-		if(validaTitulo(doc.getTitulo())== true && validaCorpo(doc.getCorpo())== true){
-			
+		if (validaTitulo(doc.getTitulo()) == true && validaCorpo(doc.getCorpo()) == true) {
+
 			return doc;
 		}
 		return null;
+	}
+
+	public ArrayList<Documento> listar(ArrayList<Documento> documentos) {
+		if (documentos != null) {
+			return documentos;
+		}
+		return null;
+
 	}
 
 }
--- test/br/edu/editor/testDocumento/TesteDocumentoService.java
+++ test/br/edu/editor/testDocumento/TesteDocumentoService.java
@@ -3,6 +3,8 @@ package br.edu.editor.testDocumento;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import br.edu.editor.model.Documento;
@@ -19,7 +21,7 @@ public class TesteDocumentoService {
 		doc.setCorpo("Corpo texto pequeno");
 		doc.setData("2019-01-22 18:32:00");
 		
-	//gfkgfjgkjfgsdflgjsdfgjsdjglsdfjg
+	
 		//ação
 		DocumentoService docServ = new DocumentoService();
 		Documento docFinal = docServ.salvar(doc);
@@ -30,6 +32,36 @@ public class TesteDocumentoService {
 		assertEquals(docFinal.getTitulo(), doc.getTitulo());
 		
 		
+	}
+	@Test
+	public void esperadoListaTodosDocumentos() {
+		//cenario
+		ArrayList<Documento> documentos = new ArrayList<>();
+		Documento doc1 = new Documento();
+		doc1.setId((long)1);
+		doc1.setTitulo("texto titulo pequeno");
+		doc1.setCorpo("Corpo texto pequeno");
+		doc1.setData("2019-01-22 18:32:00");
+		
+		Documento doc = new Documento();
+		doc.setId((long)2);
+		doc.setTitulo("texto titulo grande 123456487979/*");
+		doc.setCorpo("Corpo texto grande312111131321321 ");
+		doc.setData("2019-01-22 15:33:00");
+		
+//		documentos.add(doc);
+		documentos.add(doc1);
+		
+				
+		//acao
+		
+		DocumentoService docServ = new DocumentoService();
+		docServ.listar(documentos);
+		
+		
+		//teste
+		
+		assertEquals(0,documentos.size());
 	}
 	
 
